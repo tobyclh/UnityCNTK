b02d429887dde5e06a6af3949d6952cdad229520
Toby the potato <tobyclh@gmail.com>
2017-05-19 07:11:48 +0900

    Add main windows for the project

---
+++ Assets/MainWindow.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+namespace DeepUnity
+{
+    public class MainWindow : EditorWindow
+    {
+        public Model _profile;
+
+
+        [MenuItem("Window/SubstanceBaker/Main Panel")]
+        public static void Init()
+        {
+            EditorWindow.GetWindow(typeof(MainWindow)).Show();
+        }
+
+        void OnGUI()
+        {
+
+        }
+
+        [MenuItem("CONTEXT/ProceduralMaterial/Bake And Replace")]
+        private static void BakeAndReplace(MenuCommand menuCommand)
+        {
+        }
+
+        [MenuItem("CONTEXT/ProceduralMaterial/Bake Without Replace")]
+        private static void BakeNoReplace(MenuCommand menuCommand)
+        {
+        }
+
+
+
+
+
+    }
+}
