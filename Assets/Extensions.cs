fb844edf00f71c6ca8d064c3761ba2e72a2cc7ce
Toby the potato <tobyclh@gmail.com>
2017-05-19 09:06:13 +0900

    Add alpha channel support

--- Assets/Extensions.cs
+++ Assets/Extensions.cs
@@ -8,14 +8,16 @@ namespace DeepUnity
 {
     public static class Extensionas
     {
-        public static Value Create(this Value value, Texture2D texture)
+        public static Value Create(this Value value, Texture2D texture, bool useAlpha = false)
         {
-            var shape = NDShape.CreateNDShape(new int[] { texture.width, texture.height, 3 });
+            var shape = NDShape.CreateNDShape(new int[] { useAlpha ? 4 : 3, texture.width, texture.height });
             var rawData = texture.GetRawTextureData();
             NDArrayViewPtrVector pVec = new NDArrayViewPtrVector(rawData);
             var inputVal = Value.Create(shape, pVec, new BoolVector(), DeviceDescriptor.CPUDevice, false, false);
             return inputVal;
         }
+
+        
     }
 
 }
