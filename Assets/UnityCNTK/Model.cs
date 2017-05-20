014435bf4f5ad4dbdbe73b2a1388ce93db703727
Toby the potato <tobyclh@gmail.com>
2017-05-20 14:18:29 +0900

    Add everything else

---
+++ Assets/UnityCNTK/Model.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+using CNTK;
+using System.Timers;
+namespace UnityCNTK
+{
+    public class Model : ScriptableObject
+    {   
+        public Function function;
+        public System.DateTime CreatedTime;
+    }
+}
