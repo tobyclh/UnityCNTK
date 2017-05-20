001603cff6df6099bd459201158df240af09bcdb
Toby the potato <tobyclh@gmail.com>
2017-05-20 14:17:02 +0900

    Add synthesis Model

---
+++ Assets/UnityCNTK/SynthesisModel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+using CNTK;
+using System;
+namespace UnityCNTK
+{
+    public class SynthesisModel : Model
+    {
+        public Texture2D textureReference;
+        
+        public string namingRules;
+        public Texture2D ApplyFunctionTexture2D()
+        {
+            throw new NotImplementedException("DO IT NOW");
+        }
+    }
+}
