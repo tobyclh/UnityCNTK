d3c93b239dd6433c1ef843d44ea153dcd7d97247
Toby the potato <tobyclh@gmail.com>
2017-05-21 17:27:26 +0900

    Add value to texture conversion

--- Assets/UnityCNTK/Convert.cs
+++ Assets/UnityCNTK/Convert.cs
@@ -10,25 +10,16 @@ namespace UnityCNTK
     {
         public static CNTK.Value ToValue(Texture2D texture, bool useAlpha = false)
         {
-            //Non-copying way to create a value
-            var shape = NDShape.CreateNDShape(new int[] { useAlpha ? 4 : 3 * texture.width * texture.height });
+            var shape = NDShape.CreateNDShape(new int[] { useAlpha ? 4 : 3, texture.width, texture.height });
             var pixels = texture.GetPixels();
             var channelPixelCount = texture.width * texture.height;
             var pixelArray = new float[channelPixelCount * (useAlpha ? 4 : 3)];
             Array.Copy(Array.ConvertAll(pixels, p => (float)p.r), 0, pixelArray, 0, channelPixelCount);
             Array.Copy(Array.ConvertAll(pixels, p => (float)p.g), 0, pixelArray, channelPixelCount, channelPixelCount);
-            Array.Copy(Array.ConvertAll(pixels, p => (float)p.b), 0, pixelArray, channelPixelCount*2, channelPixelCount);
-            if (useAlpha) Array.Copy(Array.ConvertAll(pixels, p => (float)p.a), 0, pixelArray, channelPixelCount*3, channelPixelCount);
-            List<float[]> list = new List<float[]>(){pixelArray};
-
-            
-
-            // var array = new float[useAlpha ? 4 : 3, texture.width * texture.height];
-            
-
-            // NDArrayView arrayView = new NDArrayView()
-
-            var inputVal = CNTK.Value.Create(shape, list, new bool[]{true}, DeviceDescriptor.CPUDevice, false);
+            Array.Copy(Array.ConvertAll(pixels, p => (float)p.b), 0, pixelArray, channelPixelCount * 2, channelPixelCount);
+            if (useAlpha) Array.Copy(Array.ConvertAll(pixels, p => (float)p.a), 0, pixelArray, channelPixelCount * 3, channelPixelCount);
+            List<float[]> list = new List<float[]>() { pixelArray };
+            var inputVal = CNTK.Value.Create(shape, list, new bool[] { true }, DeviceDescriptor.GPUDevice(0), false);
             return inputVal;
         }
 
@@ -36,7 +27,25 @@ namespace UnityCNTK
         {
             return Convert.ToValue(mat.mainTexture as Texture2D);
         }
+        // Summary:
+        //     ///
+        //     Create a new empty texture.
+        //     ///
+        //
+        // Parameters:
+        //   value: sadasda
 
+        public static List<Texture2D> ToTexture2D(this Value value)
+        {
+            List<Texture2D> texs = new List<Texture2D>();
+            var dimemsions = value.Shape.Dimensions;
+            for(int i = 0; i < dimemsions[3]; i++)
+            {
+                Texture2D texture = new Texture2D(dimemsions[1], dimemsions[2]);
+
+            }
+            return texs;
+        }
     }
 
 }
