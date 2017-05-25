6578283965473dd161b7bd14c5d504c43bf491dc
Toby the potato <tobyclh@gmail.com>
2017-05-25 19:43:35 +0900

    Add description

--- Assets/UnityCNTK/Model.cs
+++ Assets/UnityCNTK/Model.cs
@@ -6,18 +6,22 @@ using UnityEngine;
 using UnityEditor;
 using CNTK;
 using System.Timers;
-using CNTK.Extensibility.Managed;
+using CNTK;
 namespace UnityCNTK
 {
+    // Unifying class that combine the use of both EvalDLL and CNTKLibrary
+    // The reason we have not dropped support for EvalDLL is that it expose more native function of the full API,
+    // which are very useful in general.
     public abstract class Model : ScriptableObject
     {   
         public Function function;
+        
 
         public Object input;
         public Object output;
         public abstract void LoadModel();
 
-        public abstract void Evaluate();
+        public abstract void Evaluate(DeviceDescriptor device);
 
     }
 
