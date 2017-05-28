c7aa14596f155e886b383aedf879d7200831ca4d
Toby the potato <tobyclh@gmail.com>
2017-05-28 16:33:08 +0900

    Add auto select device

--- Assets/UnityCNTK/Scripts/CNTKManager.cs
+++ Assets/UnityCNTK/Scripts/CNTKManager.cs
@@ -1,14 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using CNTK;
 namespace UnityCNTK{
 	public class CNTKManager : MonoBehaviour {
 		public static CNTKManager instance;
 		public List<Model> managedModels = new List<Model>();
+		public static DeviceDescriptor device;
+		public enum HardwareOptions
+		{
+			Auto, CPU, GPU, Default
+		}
+		[Tooltip("Avoid Default unless you are familiar with the usage")]
+		public HardwareOptions hardwareOption = HardwareOptions.Auto;
+
+		
 		
 		void Start () {
 			MakeSingleton();
+			switch (hardwareOption)
+			{
+				case HardwareOptions.Auto:
+				{
+					SelectBestDevice();
+					break;	
+				}
+				case HardwareOptions.CPU:
+				{
+					device = DeviceDescriptor.CPUDevice;
+					break;	
+				}
+				case HardwareOptions.GPU:
+				{
+					device = DeviceDescriptor.GPUDevice(0);
+					break;
+				}
+				case HardwareOptions.Default:
+				{
+					device = DeviceDescriptor.UseDefaultDevice();
+					break;
+				}
+			}
+			foreach(var model in managedModels)
+			{
+				if(model.LoadOnStart)
+				{
+					model.LoadModel();
+				}
+			}
 		}
 		
 		// Update is called once per frame
@@ -27,6 +66,19 @@ namespace UnityCNTK{
 			else
 			{
 				instance = this;
+			}
+		}
+
+		private void SelectBestDevice()
+		{
+			var gpu = DeviceDescriptor.GPUDevice(0);
+			if(gpu != null)
+			{
+				device = gpu;
+			}
+			else
+			{
+				device = DeviceDescriptor.CPUDevice;
 			}
 		}
 	}
