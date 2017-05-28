472f86896e50d6e4d3e8c24ca7e21fce78afd53a
Toby the potato <tobyclh@gmail.com>
2017-05-28 22:16:44 +0900

    Add datasource

---
+++ Assets/UnityCNTK/DataSource.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataSource : MonoBehaviour {
+
+    public UnityEngine.Object source;
+    public enum PredefinedSource
+    {
+        none, position, rotation, quaternion
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
