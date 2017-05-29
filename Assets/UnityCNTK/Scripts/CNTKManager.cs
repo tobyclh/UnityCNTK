873278d4a4af7b9a959d5be5259d9f09c7d0dde3
Toby the potato <tobyclh@gmail.com>
2017-05-29 20:55:16 +0900

    Add startstreaming method

--- Assets/UnityCNTK/Scripts/CNTKManager.cs
+++ Assets/UnityCNTK/Scripts/CNTKManager.cs
@@ -1,89 +1,104 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.Assertions;
 using CNTK;
-namespace UnityCNTK{
-	public class CNTKManager : MonoBehaviour {
-		public static CNTKManager instance;
-		public List<Model> managedModels = new List<Model>();
-		public static DeviceDescriptor device;
-		public enum HardwareOptions
-		{
-			Auto, CPU, GPU, Default
-		}
-		[Tooltip("Where to put all the work, Avoid 'Default' unless you are familiar with the usage")]
-		public HardwareOptions hardwareOption = HardwareOptions.Auto;
-
-		
-		void Awake()
-		{
-			MakeSingleton();
-		}
-		
-		void Start () {
-			switch (hardwareOption)
-			{
-				case HardwareOptions.Auto:
-				{
-					SelectBestDevice();
-					break;	
-				}
-				case HardwareOptions.CPU:
-				{
-					device = DeviceDescriptor.CPUDevice;
-					break;	
-				}
-				case HardwareOptions.GPU:
-				{
-					device = DeviceDescriptor.GPUDevice(0);
-					break;
-				}
-				case HardwareOptions.Default:
-				{
-					device = DeviceDescriptor.UseDefaultDevice();
-					break;
-				}
-			}
-			foreach(var model in managedModels)
-			{
-				if(model.LoadOnStart)
-				{
-					model.LoadModel();
-				}
-			}
-		}
-		
-		// Update is called once per frame
-		void Update () {
-			
-		}
+namespace UnityCNTK
+{
+    public class CNTKManager : MonoBehaviour
+    {
+        public static CNTKManager instance;
+        public List<Model> managedModels = new List<Model>();
+        public List<int> streamingModels = new List<int>();
+        public static DeviceDescriptor device;
+        public enum HardwareOptions
+        {
+            Auto, CPU, GPU, Default
+        }
+        [Tooltip("Where to put all the work, Avoid 'Default' unless you are familiar with the usage")]
+        public HardwareOptions hardwareOption = HardwareOptions.Auto;
 
 
-		private void MakeSingleton()
-		{
-			if (instance != null && instance != this )
-			{	
-				Debug.LogError("Only 1 CNTK manager allowed in a scene, destroying");
-				Destroy(this);
-			}
-			else
-			{
-				instance = this;
-			}
-		}
+        void Awake()
+        {
+            MakeSingleton();
+        }
 
-		private void SelectBestDevice()
-		{
-			var gpu = DeviceDescriptor.GPUDevice(0);
-			if(gpu != null)
-			{
-				device = gpu;
-			}
-			else
-			{
-				device = DeviceDescriptor.CPUDevice;
-			}
-		}
-	}
+        void Start()
+        {
+            switch (hardwareOption)
+            {
+                case HardwareOptions.Auto:
+                    {
+                        SelectBestDevice();
+                        break;
+                    }
+                case HardwareOptions.CPU:
+                    {
+                        device = DeviceDescriptor.CPUDevice;
+                        break;
+                    }
+                case HardwareOptions.GPU:
+                    {
+                        device = DeviceDescriptor.GPUDevice(0);
+                        break;
+                    }
+                case HardwareOptions.Default:
+                    {
+                        device = DeviceDescriptor.UseDefaultDevice();
+                        break;
+                    }
+            }
+            for (int i = 0; i < managedModels.Count; i++)
+            {
+                var model = managedModels[i];
+                if (model.LoadOnStart)
+                {
+                    model.LoadModel();
+                }
+            }
+        }
+
+        void Update()
+        {
+
+        }
+
+
+        private void MakeSingleton()
+        {
+            if (instance != null && instance != this)
+            {
+                Debug.LogError("Only 1 CNTK manager allowed in a scene, destroying");
+                Destroy(this);
+            }
+            else
+            {
+                instance = this;
+            }
+        }
+
+        private void SelectBestDevice()
+        {
+            var gpu = DeviceDescriptor.GPUDevice(0);
+            if (gpu != null)
+            {
+                device = gpu;
+            }
+            else
+            {
+                device = DeviceDescriptor.CPUDevice;
+            }
+        }
+
+        public void StartStreaming(string modelName)
+        {
+            //calling single ensure there is only 1 
+            var model = managedModels.Where(x => x.name == modelName).Single();
+            var streamingModel = model as StreamingModel;
+            StartCoroutine(streamingModel.StartStreaming());
+        }
+    }
 
 }
