a8b1751a5aaed9c95effa2b8d285378e99202f4c
Toby the potato <tobyclh@gmail.com>
2017-06-01 13:11:46 +0900

    Make fer+ model script call evaluation

--- Assets/UnityCNTK/Scripts/Models/FERPlusModel.cs
+++ Assets/UnityCNTK/Scripts/Models/FERPlusModel.cs
@@ -8,26 +8,26 @@ using UnityEngine.Assertions;
 
 namespace UnityCNTK
 {
-    
+
     public class FERPlusModel : Model<Texture2D, int>, IStreamModel
     {
-		[Tooltip("Evaluation carry out in every specificed second")]
-		public float evaluationPeriod = 10;
+        [Tooltip("Evaluation carry out in every specificed second")]
+        public float evaluationPeriod = 10;
         private bool shouldStop = false;
         private bool isStreaming = false;
-        
+        public new Texture2DSource dataSource;
         public void StartStreaming()
         {
             Assert.IsFalse(isStreaming, " is already streaming");
             Debug.Log("Start Streaming");
-            //InvokeRepeating("MyStream", 0, evaluationPeriod);
+            InvokeRepeating("MyStream", 0, evaluationPeriod);
         }
 
         private void MyStream()
         {
             Debug.Log("Grabbing data");
-            
-            //Evaluate(source);
+            var data =dataSource.GetData();
+            var s = Evaluate(data);
         }
 
         public void StopStreaming()
@@ -37,11 +37,13 @@ namespace UnityCNTK
         }
         public override int OnPostProcess(Value output)
         {
+            Debug.Log("OnPostprocess FER+");
             throw new NotImplementedException();
         }
 
         public override Value OnPreprocess(Texture2D input)
         {
+            Debug.Log("OnPreprocess FER+");
             return input.ToValue(CNTKManager.device);
         }
 
